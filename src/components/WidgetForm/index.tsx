388ca37266cd14315789191744671f77ed42f2e8
murilo muller <murilomuller@protonmail.com>
2022-09-06 11:45:30 -0300

    Feedbacktypestep configurado

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,60 @@
+import { CloseButton } from '../CloseButton'
+
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { useState } from 'react'
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de uma barata'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lampada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de uma ferramenta adicional'
+    }
+  }
+}
+
+export type FeedBackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null)
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+   
+
+      {!feedbackType ? (
+       <FeedbackTypeStep onFeedbackChanged={setFeedbackType} />
+      ) : (
+         <FeedbackContentStep />
+      )}
+
+      <footer>
+        Feito com 💛 pelo
+        <a
+          className="underline underline-offset-2"
+          href="https://github.com/mullerhub"
+        >
+          MullerHub
+        </a>
+      </footer>
+    </div>
+  )
+}
