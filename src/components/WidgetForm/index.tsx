f0a676828a63a82483a228ffc00386b8250f6cfc
murilo muller <murilomuller@protonmail.com>
2022-09-06 12:23:00 -0300

    types step finished

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -43,7 +43,7 @@ export function WidgetForm() {
       {!feedbackType ? (
        <FeedbackTypeStep onFeedbackChanged={setFeedbackType} />
       ) : (
-         <FeedbackContentStep />
+         <FeedbackContentStep feedbackType={feedbackType} />
       )}
 
       <footer>
